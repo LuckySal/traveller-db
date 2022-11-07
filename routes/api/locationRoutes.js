34ba0a8b08d7857e4f852b47de8aae856ffda097
Squiddy <wolaki96@gmail.com>
2022-11-07 12:38:06 -0500

    added data to location.js

--- routes/api/locationRoutes.js
+++ routes/api/locationRoutes.js
@@ -2,9 +2,34 @@ const router = require('express').Router();
 const {} = require();
 
 //get all locations
-
+router.get('./', async (req, res) => {
+    try {
+        const locationData = await Location.findAll ({
+            include: [{model: Traveller }],
+        });
+        res.status(200).json(locationData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 // get a single location
-
+router.get('/:id', async (req, res) => {
+    try {
+      const locationData = await Location.findByPk(req.params.id, {
+        include: [{ model: Traveller }],
+      });
+  
+      if (!locationData) {
+        res.status(404).json({ message: 'No location found with that id!' });
+        return;
+      }
+  
+      res.status(200).json(locationData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+       
 // create a location
 
 // delete a location
